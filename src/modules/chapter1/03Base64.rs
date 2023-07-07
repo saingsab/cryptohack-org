1e4a350aef70ed996e5554d8563c68e40b4eb57b
Ayoung <saing.sab@gmail.com>
2023-07-07 18:14:27 +0700

    Bytes and Big Integers

--- src/modules/chapter1/03Base64.rs
+++ src/modules/chapter1/03Base64.rs
@@ -4,5 +4,5 @@ fn base64ToData() {
     let input = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf";
     let decoded = hex::decode(input).expect("Decoding failed");
 
-    println!("{:}", general_purpose::STANDARD.encode(&decoded));
+    println!("{* Here is your flag: }", general_purpose::STANDARD.encode(&decoded));
 }
