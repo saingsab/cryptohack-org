8ec489285bf91e7e7a82bad93a619cbda0ed5277
Ayoung <saing.sab@gmail.com>
2023-07-07 10:47:51 +0700

    level 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ascii_converter::*;
+use base64::{engine::general_purpose, Engine as _};
 fn main() {
     // let decoded_string = hex::decode("63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d");
     // let v = vec![decoded_string];
@@ -9,11 +10,13 @@ fn main() {
     //     Err(e) => println!("* Error: {}", e),
     // }
     // println!("{:?}", v1);
-    let input = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d";
+    let input = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf";
     let decoded = hex::decode(input).expect("Decoding failed");
 
-    match decimals_to_string(&decoded) {
-        Ok(num) => println!("* Output: {}", num),
-        Err(e) => println!("* Error: {}", e),
-    }
+    println!("{:}", general_purpose::STANDARD.encode(&decoded));
+
+    // match decimals_to_string(&decoded) {
+    //     Ok(num) => println!("* Output: {}", num),
+    //     Err(e) => println!("* Error: {}", e),
+    // }
 }
---
+++ src/modules/chapter1/03Base64.rs
@@ -0,0 +1,8 @@
+use base64::{engine::general_purpose, Engine as _};
+
+fn base64ToData() {
+    let input = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf";
+    let decoded = hex::decode(input).expect("Decoding failed");
+
+    println!("{:}", general_purpose::STANDARD.encode(&decoded));
+}
