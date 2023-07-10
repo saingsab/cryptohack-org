6527081bfeb9d58be8f063a1d281d3894c3171c8
Ayoung <saing.sab@gmail.com>
2023-07-10 23:08:09 +0700

    XOR

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use ascii_converter::*;
+use core::str;
+use std::str::FromStr;
+use hex::{FromHex, ToHex};
 
 fn convert_ascii_to_string(_v: Vec<u8>) {
     match decimals_to_string(&_v) {
@@ -16,14 +19,18 @@ fn convert_hex_bytes_to_ascii(_v: &str) {
     println!("{:?}", hex)
 }
 
-fn convert_hex_to_decimals(_v: &str){
-    
+fn convert_hex_to_decimals(_v: &str) {
+    let buffer: [u8; 12] = <[u8; 12]>::from_hex([0x48, 0x45, 0x4c, 0x4c, 0x4f]).expect("something went wrong");
+    let string = str::from_utf8(&buffer).expect("invalid buffer length");
+
+    println!("{}", string); 
 }
+
 fn main() {
 
     // let v = vec![72, 69, 76, 76, 79];
     // let v = vec![0x48, 0x45, 0x4c, 0x4c, 0x4f];
     let input = "48454c4c4f";
-    // convert_hex_bytes_to_ascii(input);
-    convert_hex_to_decimals(input);
+    ;
+    convert_hex_to_decimals("d");
 }
