4b6e756352252d64dd733449ca069cef19753518
Ayoung <saing.sab@gmail.com>
2023-07-25 17:26:45 +0700

    xor

--- src/main.rs
+++ src/main.rs
@@ -3,60 +3,15 @@ use core::str;
 use std::str::FromStr;
 use hex::{FromHex, ToHex};
 
-fn convert_ascii_to_string(_v: Vec<u8>) {
-    match decimals_to_string(&_v) {
-        Ok(num) => println!("* Here is your flag: {}", num),
-        Err(e) => println!("* Error: {}", e),
-    }
-}
+// Challange
+// Given the string label, XOR each character with the integer 13. Convert these integers back to a string and submit the flag as crypto{new_string}.
 
-fn convert_hex_bytes_to_ascii(_v: &str) {
-    /*
-    ascii bytes: [72, 69, 76, 76, 79]
-    hex bytes: [0x48, 0x45, 0x4c, 0x4c, 0x4f]
-    */
-    let hex = hex::decode(_v).expect("Decoding failed");
-    println!("{:?}", hex)
-}
-
-fn convert_hex_to_decimals(_v: &str) {
-    let buffer: [u8; 12] = <[u8; 12]>::from_hex([0x48, 0x45, 0x4c, 0x4c, 0x4f]).expect("something went wrong");
-    let string = str::from_utf8(&buffer).expect("invalid buffer length");
-
-    println!("{}", string); 
-}
-
-// fn main() {
-
-//     // let v = vec![72, 69, 76, 76, 79];
-//     // let v = vec![0x48, 0x45, 0x4c, 0x4c, 0x4f];
-//     let input = "48454c4c4f";
-//     ;
-//     convert_hex_to_decimals("d");
-// }
-fn three_vowels(word: &str) -> bool {
-    let mut vowel_count = 0;
-    for c in word.chars() {
-        match c {
-            'a' | 'e' | 'i' | '0' | 'u' => {
-               vowel_count += 1;
-               if vowel_count >= 3 {
-                    return true;
-               } 
-            }
-            _ => vowel_count = 0 
-        }
-    }
-    false
-}
+// Todo
+// label -> decimal and store in vec
+// Each items in vec ^ by 13
+// Convert these integers back to a string 
 
 fn main() {
     let sentence_string = 
         "Once upon a time, there was a friendly curious crab named Ferris".to_string();
-    
-    for word in sentence_string.split(' ') {
-        if three_vowels(word) {
-            println!("{} has three consecutive vowels!", word);
-        }
-    }
 }
